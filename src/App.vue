63ec934c963e7ba5a9ff2bc136a36220c2d7fd79
mavis0 <646357406@qq.com>
2021-05-09 22:53:14 +0800

    图标动效

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img :class="{'img-done': isDone}" src="./assets/IMG-novaeangliae.jpg" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => {
+    return {
+        isDone: false 
+    }
+  },
+  mounted: function(){
+    setTimeout(() => {
+        this.isDone = true;
+    }, 1000);
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -24,5 +28,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+img {
+    top: 0;
+    left: 0;
+    width: 100vw;
+    position: absolute;
+    clip-path: circle(95% at 47% 50%);
+    /* transition: all 5s cubic-bezier(0.075, 0.82, 0.165, 1); */
+    transition: all 2s linear;
+}
+
+.img-done {
+    clip-path: circle(15% at 50% 40%);
+    width: 200px;
+    /* left: 50px; */
+    left: 3%;
 }
 </style>
