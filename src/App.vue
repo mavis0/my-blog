3bddff3b88679d0acb31cc11b1651b4335c3dd89
mavis0 <646357406@qq.com>
2021-05-10 13:05:04 +0800

    nav

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <img :class="{'img-done': isDone}" src="./assets/IMG-novaeangliae.jpg" />
+    <img id="avator" :class="{'img-done': isDone, 'animate__animated animate__pulse': isBounce}" src="./assets/IMG-novaeangliae.jpg" />
+    <nav>
+        <ul>
+            <li>HOME</li>
+            <li>ARTICLE</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+        </ul>
+    </nav>
   </div>
 </template>
 
@@ -10,24 +18,28 @@ export default {
   name: 'App',
   data: () => {
     return {
-        isDone: false 
+        isDone: false,
+        isBounce: false
     }
   },
-  mounted: function(){
+  mounted: function() {
     setTimeout(() => {
         this.isDone = true;
+        let avator = document.getElementById('avator');
+        avator.addEventListener("transitionend", () => {
+          this.isBounce = true;
+        })
     }, 1000);
   }
 }
 </script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 40px;
 }
 
 img {
@@ -43,7 +55,22 @@ img {
 .img-done {
     clip-path: circle(15% at 50% 40%);
     width: 200px;
-    /* left: 50px; */
     left: 3%;
+}
+
+nav {
+    margin: 0 auto;
+    width: max-content;
+}
+
+ul {
+    list-style-type: none;
+    display: inline-flex;
+    justify-content: center;
+}
+
+li {
+    display: inline-block;
+    margin: 0 20%;
 }
 </style>
